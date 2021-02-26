747a3007af60448fc88181850f470e6959ce5eb6
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-02-26 13:23:51 -0600

    Update app.js

--- app.js
+++ app.js
@@ -19,13 +19,7 @@ function randomSquare() {
 }
 
 
-function fondo(){
-  var sonido = document.createElement("iframe");
-	sonido.setAttribute("src","sounds/theme.mp3");
-	document.body.appendChild(sonido);
-	 	
 
-}
 
 function sonarPajaros(hitPosition){
 	var sonido = document.createElement("iframe");
